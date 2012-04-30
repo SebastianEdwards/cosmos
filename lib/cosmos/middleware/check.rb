ef1c2e5a8a71768c29cf3de3b608170b4fa33776
Sebastian Edwards <s.thursday@gmail.com>
2012-04-30 14:34:33 +1200

    Add check middleware.

---
+++ lib/cosmos/middleware/check.rb
@@ -0,0 +1,36 @@
+module Cosmos
+  class FailedCheckError < StandardError
+    attr_reader :check_type
+
+    def initialize(check_type)
+      @check_type = check_type
+    end
+  end
+
+  module Middleware
+    class Check
+      def initialize(app, type, error = FailedCheckError)
+        @app    = app
+        @type   = type
+        @error =  error
+      end
+
+      def has_one_item?
+        has_items? && @subject.items.length == 1
+      end
+
+      def has_items?
+        @subject.respond_to? :items
+      end
+
+      def call(env)
+        @subject = env[:current_body]
+        if respond_to? @type
+          success = send(@type)
+          raise @error.new(@type) unless success
+        end
+        @app.call env
+      end
+    end
+  end
+end
